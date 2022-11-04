736cf97fe9ec13bb88caf14597c54544cb1c1048
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-11-03 22:45:00 -0700

    fix routing issues on product and tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -40,20 +40,12 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new tag
   try {
-    const tagData = await Tag.findByPk(req.params.id, {
-      // Add B as a second model to JOIN with
-      include: [{ model: Product }],
-    });
-
-    if (!tagData) {
-      res.status(404).json({ message: 'No tag found with that id!' });
-      return;
-    }
-
+    const tagData = await Tag.create(req.body);
     res.status(200).json(tagData);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(400).json(err);
   }
+  
 });
 
 router.put('/:id', async (req, res) => {
@@ -85,8 +77,8 @@ router.delete('/:id', async (req, res) => {
       },
     });
 
-    if (!TagData) {
-      res.status(404).json({ message: 'No category found with that id!' });
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with that id!' });
       return;
     }
 
@@ -97,3 +89,19 @@ router.delete('/:id', async (req, res) => {
 });
 
 module.exports = router;
+
+// try {
+//   const tagData = await Tag.findByPk(req.params.id, {
+//     // Add B as a second model to JOIN with
+//     include: [{ model: Product }],
+//   });
+
+//   if (!tagData) {
+//     res.status(404).json({ message: 'No tag found with that id!' });
+//     return;
+//   }
+
+//   res.status(200).json(tagData);
+// } catch (err) {
+//   res.status(500).json(err);
+// }
